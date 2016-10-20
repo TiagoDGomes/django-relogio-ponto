c2a1679190adf9d8f7c233b7f59956b06cde826e
Tiago <tiago.gomes@ifsp.edu.br>
2016-10-20 10:19:22 -0200

    add pyRelogioPonto

--- core/models.py
+++ core/models.py
@@ -2,4 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=100)
+    pis = models.CharField(max_length=25, unique=True)
+    verificar_digital = models.NullBooleanField()
+
+class Matricula(models.Model):
+    colaborador = models.ForeignKey(Colaborador)
+    numero = models.IntegerField()
+
+
+    
+    
