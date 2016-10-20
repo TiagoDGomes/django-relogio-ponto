114190dae66f3214dbc0ac69767210a94305e3a6
Tiago <tiago.gomes@ifsp.edu.br>
2016-10-20 15:54:59 -0200

    admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from core.models import RelogioPonto, Colaborador, Parametro
 
-# Register your models here.
+class ParametroInline(admin.StackedInline):
+    model = Parametro
+    extra = 0
+    max_num = 0
+
+@admin.register(RelogioPonto)
+class RelogioPontoAdmin(admin.ModelAdmin):
+    inlines = [ParametroInline,]
+    
+    def get_inline_instances(self, request, obj=None):
+        if obj:
+            return [inline(self.model, self.admin_site) for inline in self.inlines]
+        else:
+            return []
+        
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        if not object_id:
+            extra_context['show_save'] = False
+        
+        return super(RelogioPontoAdmin, self).change_view(request, object_id,
+            form_url, extra_context=extra_context)
+        
+@admin.register(Colaborador)
+class ColaboradorAdmin(admin.ModelAdmin):
+    pass
--- core/models.py
+++ core/models.py
@@ -1,16 +1,61 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from pyRelogioPonto.relogioponto.base import get_rep_suportados
+
 
 class Colaborador(models.Model):
     nome = models.CharField(max_length=100)
     pis = models.CharField(max_length=25, unique=True)
     verificar_digital = models.NullBooleanField()
+    
+    class Meta:
+        verbose_name_plural = 'Colaboradores'
 
 class Matricula(models.Model):
     colaborador = models.ForeignKey(Colaborador)
-    numero = models.IntegerField()
+    numero = models.IntegerField()   
+    
+class RelogioPonto(models.Model):      
+    
+    CHOICES_TIPOS_RELOGIOS = [(id, nome) for id, nome, tipo, parametros in get_rep_suportados()]  
+    nome = models.CharField(max_length=30)
+    tipo = models.IntegerField(choices=CHOICES_TIPOS_RELOGIOS)
+    
+    class Meta:
+        verbose_name = 'relógio de ponto'
+        verbose_name_plural = 'relógios de ponto'
+    
+    def __str__(self):
+        return self.nome
+    
+    def save(self, *args, **kwargs):
+        if self.id:
+            nao_salvo = True
+        
+        s = super(RelogioPonto, self).save(*args, **kwargs)
+        for id, nome, tipo, parametros in get_rep_suportados():
+            print(nome, tipo)
+            if self.tipo == id:
+                for propriedade, tipo_valor in parametros:
+                    print(propriedade)
+                    parametro = Parametro()
+                    parametro.propriedade =  propriedade
+                    parametro.tipo = tipo_valor
+                    parametro.relogio = self
+                    parametro.save()  
+        return s
+            
+       
 
+class Parametro(models.Model):
+    propriedade = models.CharField(max_length=25, editable=False)
+    valor = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=25, default='str', editable=False)
+    relogio = models.ForeignKey(RelogioPonto)
+    
+    def __str__(self):
+        return self.propriedade
 
     
     
