6fdc74c1c63d329d58bc0d769f7e1a9cef4d8c61
Tiago <tiago.gomes@ifsp.edu.br>
2016-10-20 07:05:50 -0200

    fix nome de arquivo

--- core/forms.py
+++ core/forms.py
@@ -11,5 +11,6 @@ class GerarArquivoForm(forms.Form):
     
     @property
     def nome_arquivo(self):
-        return "{1}-{2}".format( self.cleaned_data('inicio').strptime('%d%m%Y'), self.cleaned_data('fim').strptime('%d%m%Y'))
+        print (self.cleaned_data)
+        return "{0}-{1}".format( self.cleaned_data['inicio'].strftime('%d%m%Y'), self.cleaned_data['fim'].strftime('%d%m%Y'))
         
