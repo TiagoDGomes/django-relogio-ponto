69e42bbc925968016edbed64377d0e750dcf1282
Tiago <tiago.gomes@ifsp.edu.br>
2016-11-23 07:14:10 -0200

    Nome da app 'core'

--- core/apps.py
+++ core/apps.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
@@ -5,3 +6,4 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
+    verbose_name = "Sistema de ponto eletrônico"
